ccbf85d79293f026e0f605a06576b38576ef8203
Elazar Cohen <elazarcoh@gmail.com>
2023-11-08 16:17:16 +0200

    add yew

--- src/webview-ui/src/lib.rs
+++ src/webview-ui/src/lib.rs
@@ -1,8 +1,24 @@
+mod app;
+
 use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
 fn run() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
     let window = web_sys::window().expect("no global `window` exists");
@@ -14,6 +30,10 @@ fn run() -> Result<(), JsValue> {
     val.set_text_content(Some("Hello from Rust!"));
 
     body.append_child(&val)?;
+
+    console::log_1(&"Hello using web-sys".into());
+
+    app::render();
 
     Ok(())
 }

f8e4bfbfa8c809c2d6b5e7ed9d57f46a288f985b
Elazar <elazarcoh@gmail.com>
2025-01-30 11:47:30 +0200

    input component

--- src/webview-ui/src/components/view_container.rs
+++ src/webview-ui/src/components/view_container.rs
@@ -10,7 +10,7 @@ use crate::{
     application_state::{app_state::AppState, images::ImageAvailability},
     coloring::{self, Coloring, DrawingOptions},
     colormap,
-    common::{ViewId, ViewableObjectId},
+    common::{Channels, ViewId, ViewableObjectId},
     components::{
         legend::Legend, spinner::Spinner, viewable_info_container::ViewableInfoContainer,
     },
@@ -30,6 +30,106 @@ fn get_segmentation_colormap(
         .get(name)
         .ok_or(anyhow::anyhow!("Color map {} not found", name));
     colormap
+}
+
+#[derive(PartialEq, Properties)]
+pub struct ClippingInputProps {}
+
+#[function_component]
+pub fn ClippingInput(props: &ClippingInputProps) -> Html {
+    let ClippingInputProps {} = props;
+    let style = use_style!(
+        r#"
+
+        .container {
+            background-color: var(--vscode-input-background);
+            position: relative;
+            display: block;
+            overflow: hidden;
+            border-radius: 0.375rem;
+            border: 1px solid #e2e8f0;
+            padding-left: 0.75rem;
+            padding-right: 0.75rem;
+            padding-top: 0.5rem;
+            --tw-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+            --tw-shadow-colored: 0 1px 2px 0 var(--tw-shadow-color);
+            box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+        }
+
+        .container:focus-within {
+            --tw-border-opacity: 1;
+            border-color: rgb(37 99 235 / var(--tw-border-opacity, 1)) /* #2563eb */;
+        }
+
+        .container input {
+            background-color: transparent;
+            color: var(--vscode-input-foreground);
+            height: 2rem;
+            width: 100%;
+            border: none;
+            padding: 0;
+        }
+
+        .container input::placeholder {
+            color: transparent;
+        }
+
+        .container input:focus {
+            border-color: transparent;
+            outline: 2px solid transparent;
+            outline-offset: 2px;
+        }
+
+        .container span {
+            position: absolute;
+            inset-inline-start: 0.75rem;
+            top: 0.75rem;
+            transform: translateY(-50%);
+            font-size: 0.6rem;
+            line-height: 1rem;
+            color: #9ca3af;
+            transition-property: all;
+            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+            transition-duration: 150ms;
+        }
+
+        .container .peer:placeholder-shown ~ .peer-placeholder-shown {
+            top: 50%;
+            font-size: 0.75rem;
+            line-height: 1.25rem;
+        }
+
+        .container .peer:focus ~ .peer-focus {
+            top: 0.75rem;
+            font-size: 0.6rem;
+            line-height: 1rem;
+        }
+
+        "#,
+    );
+    html! {
+        <div class={style}>
+            <div>{"Clipping"}</div>
+
+            <label
+                for="UserEmail"
+                class="container"
+            >
+                <input
+                    type="email"
+                    id="UserEmail"
+                    placeholder="Email"
+                    class="peer"
+                />
+
+                <span
+                    class="peer-placeholder-shown peer-focus"
+                    >
+                    {"Email"}
+                </span>
+            </label>
+        </div>
+    }
 }
 
 fn make_info_items(
@@ -76,7 +176,7 @@ fn make_info_items(
                             colormap.as_ref(),
                             drawing_options,
                         );
-                        
+
                         (color_zero_one * 255.0).xyz().to_array()
                     })
                     .collect::<Vec<_>>();
@@ -95,6 +195,12 @@ fn make_info_items(
                     />
                 });
             }
+        }
+
+        if texture.info.channels == Channels::One {
+            info_items.push(html! {
+                <ClippingInput />
+            });
         }
     }
 
@@ -179,7 +285,7 @@ pub(crate) fn ViewContainer(props: &ViewContainerProps) -> Html {
             </div>
             if !info_items.is_empty() {
                 <div class={info_container_style}>
-                    <ViewableInfoContainer collapsed={true}>
+                    <ViewableInfoContainer collapsed={false}>
                         {info_items}
                     </ViewableInfoContainer>
                 </div>

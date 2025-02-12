607e005082395fe434cbcb900c181fc7eb7d7fbf
Elazar <elazarcoh@gmail.com>
2025-02-12 14:55:27 +0200

    configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    { ignores: [".node_modules/*"] },
+    eslintPluginPrettierRecommended,
 ];

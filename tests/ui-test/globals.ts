45e70ffab0f1c0007f42c56a3c3dec598db6bc80
Elazar <elazarcoh@gmail.com>
2025-09-07 23:00:34 +0300

    move test data

--- tests/ui-test/globals.ts
+++ tests/ui-test/globals.ts
@@ -2,7 +2,7 @@ import * as path from 'node:path';
 import { TitleBar, VSBrowser } from 'vscode-extension-tester';
 import { openWorkspaceFile } from './test-utils';
 
-export const WORKSPACE_DIR = path.join(__dirname, '../../..', 'python_test');
+export const WORKSPACE_DIR = path.join(__dirname, '../../..', 'tests/test-data/workspace');
 const WORKSPACE_FILE = path.join(WORKSPACE_DIR, '.vscode', 'tests.code-workspace');
 
 export async function openWorkspace(workspaceFile: string = WORKSPACE_FILE) {
